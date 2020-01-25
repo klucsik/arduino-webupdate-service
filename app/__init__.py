77f3c7a07c281eb258c19acece2e29a6f2eb3abf
KlucsikKP <klucsik.krisztian@gmail.com>
2020-01-25 19:23:46 +0100

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, url_for
+from config import Config
+import os
+app = Flask(__name__)
+
+
+@app.route('/check')
+def check_ver():
+    name = request.args.get("name")
+    installed_ver = request.args.get("ver")
+    response = ""
+    my_ver = ""
+    binary_path =""
+    dir_to_file = "bin/" + name
+    dir_to_search = Config.STATIC + dir_to_file
+    print(dir_to_search)
+    for root, dirs, files in os.walk(dir_to_search):
+        my_ver = files[0].split(".")[0]
+        binary_path = url_for('static', filename=dir_to_file + "/" + files[0])
+        print(binary_path)
+    if installed_ver != my_ver:
+        response = binary_path
+    else:
+        response = "update not needed"
+    return response
+
---
+++ config.py
@@ -0,0 +1,12 @@
+import datetime
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    BASEDIR = basedir
+    STATIC = basedir + "/app/static/"
